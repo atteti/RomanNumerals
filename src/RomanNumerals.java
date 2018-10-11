bf7c8f9f061ba8329d77e98685825c56688eaed9
attet <attetihinen@gmail.com>
2018-10-12 01:24:45 +0300

    Assignment finished.

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,206 @@
+import java.util.List;
+import java.util.ArrayList;
 
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+	
+	private static final char I = 'I';
+	private static final char V = 'V';
+	private static final char X = 'X';
+	private static final char L = 'L';
+	private static final char C = 'C';
+	private static final char D = 'D';
+	private static final char M = 'M';
 		
+	
+	
+	public int convertToInteger(final String numbers) {
+		
+		int sum = 0;
+		int index = 0;
+		int countI = 0;
+		int countX = 0;
+		int countC = 0;
+		int countM = 0;
+		int countV = 0;
+		int countL = 0;
+		int countD = 0;
+				
+		while (index < numbers.length()) {
+			
+			char nextChar = '\n';
+			
+			if (index + 1 < numbers.length()) {
+				nextChar = numbers.charAt(index + 1);
+			}
+			
+			switch (numbers.charAt(index)) {
+				case I: 
+					countI++;
+					if (sumMarkI(nextChar) == 0) {
+						return -1;
+					} else {
+						sum += sumMarkI(nextChar);
+					}
+					break;
+				case V:
+					countV++;
+					if (sumMarkV(nextChar) == 0) {
+						return -1;
+					} else {
+						sum += sumMarkV(nextChar);
+					}
+					break;
+				case X:
+					countX++;
+					if (sumMarkX(nextChar) == 0) {
+						return -1;
+					} else {
+						sum += sumMarkX(nextChar);
+					}
+					break;
+				case L:
+					countL++;
+					if (sumMarkL(nextChar) == 0) {
+						return -1;
+					} else {
+						sum += sumMarkL(nextChar);
+					}
+					break;
+				case C:
+					countC++;
+					if (sumMarkC(nextChar) == 0) {
+						return -1;
+					} else {
+						sum += sumMarkC(nextChar);
+					}
+					break;
+				case D:
+					countD++;
+					if (nextChar != '\n' && 
+						nextChar == M) {
+						return -1;
+					} else {
+						sum += 500;
+					}
+					break;
+				case M:
+					countM++;
+					sum += 1000;
+					break;
+				default:
+					return -1;
+			}
+		
+			index++;
+		}	
+			
+		if (countI > 3 || countX > 3 || countC > 3 || countM > 3) {
+			return -1;
+		}
+		
+		if (countV > 1 || countL > 1 || countD > 1) {
+			return -1;
+		}
+		
+		return sum;
+	}
+	
+	private int sumMarkI(final char character) {
+		
+		int sum = 0;
+		
+		switch (character) {
+			case V:
+			case X:
+				sum -= 1;
+				break;
+			case L:
+			case C:
+			case D:
+			case M:
+				return 0;
+				
+			default: 
+				sum += 1;
+				break;
+		}
+		
+		return sum;
+	}
+			
+	private int sumMarkV(final char character) {
+		
+		int sum = 0;
+		
+		switch (character) {
+			case X:
+			case L:
+			case C:
+			case D:
+			case M:
+				return 0;
+			default: 
+				sum += 5;
+				break;
+		}
+		
+		return sum;
+	}
+	
+	private int sumMarkX(final char character) {
+		
+		int sum = 0;
+		
+		switch (character) {
+			case L:
+			case C:
+				sum -= 10;
+				break;
+			case D:
+			case M:
+				return 0;
+			default: 
+				sum += 10;
+				break;
+		}
+		
+		return sum;
+	}
+	
+	private int sumMarkL(final char character) {
+		
+		int sum = 0;
+		
+		switch (character) {
+			case C:
+			case D:
+			case M:
+				return 0;	
+			default: 
+				sum += 50;
+				break;
+		}
+		
+		return sum;
+	}
+	
+	private int sumMarkC(final char character) {
+		
+		int sum = 0;
+		
+		switch (character) {
+			case D:
+			case M:
+				sum -= 100;
+				break;	
+			default: 
+				sum += 100;
+				break;
+		}
+		
+		return sum;
 	}
 }
+			
+		
+		
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,118 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConverToIntegerWithSingleMark() {
+		RomanNumerals numerals = new RomanNumerals();
+		assertEquals(1, numerals.convertToInteger("I"));
+		assertEquals(5, numerals.convertToInteger("V"));
+		assertEquals(10, numerals.convertToInteger("X"));
+		assertEquals(50, numerals.convertToInteger("L"));
+		assertEquals(100, numerals.convertToInteger("C"));
+		assertEquals(500, numerals.convertToInteger("D"));
+		assertEquals(1000, numerals.convertToInteger("M"));
 	}
-
+	
+	@Test
+	public void testConverToIntegerWithMoreThanSingleMark() {
+		RomanNumerals numerals = new RomanNumerals();
+		assertEquals(3, numerals.convertToInteger("III"));
+		assertEquals(4, numerals.convertToInteger("IV"));
+		assertEquals(6, numerals.convertToInteger("VI"));
+		assertEquals(13, numerals.convertToInteger("XIII"));
+		assertEquals(60, numerals.convertToInteger("LX"));
+		assertEquals(80, numerals.convertToInteger("LXXX"));
+		assertEquals(90, numerals.convertToInteger("XC"));
+		assertEquals(110, numerals.convertToInteger("CX"));
+		assertEquals(600, numerals.convertToInteger("DC"));
+		assertEquals(704, numerals.convertToInteger("DCCIV"));
+		assertEquals(1984, numerals.convertToInteger("MCMLXXXIV"));
+		assertEquals(2014, numerals.convertToInteger("MMXIV"));
+		assertEquals(3199, numerals.convertToInteger("MMMCXCIX"));
+	}
+	
+	@Test
+	public void testConverToIntegerWithTooManyMarkIorXorCorM() {
+		RomanNumerals numerals = new RomanNumerals();
+		assertEquals(-1, numerals.convertToInteger("IIII"));
+		assertEquals(-1, numerals.convertToInteger("XXXX"));
+		assertEquals(-1, numerals.convertToInteger("CCCC"));
+		assertEquals(-1, numerals.convertToInteger("MMMM"));
+	}
+	
+	@Test
+	public void testConverToIntegerWithTooManySameMarkVorLorD() {
+		RomanNumerals numerals = new RomanNumerals();
+		assertEquals(-1, numerals.convertToInteger("VV"));
+		assertEquals(-1, numerals.convertToInteger("LL"));
+		assertEquals(-1, numerals.convertToInteger("DD"));
+	}
+	
+	@Test
+	public void testConverToIntegerNextToMarkI() {
+		RomanNumerals numerals = new RomanNumerals();
+		assertEquals(-1, numerals.convertToInteger("IL"));
+		assertEquals(-1, numerals.convertToInteger("IC"));
+		assertEquals(-1, numerals.convertToInteger("ID"));
+		assertEquals(-1, numerals.convertToInteger("IM"));
+		assertEquals(4, numerals.convertToInteger("IV"));
+		assertEquals(9, numerals.convertToInteger("IX"));
+		assertEquals(2, numerals.convertToInteger("II"));
+	}
+	
+	@Test
+	public void testConverToIntegerNextToMarkX() {
+		RomanNumerals numerals = new RomanNumerals();
+		assertEquals(-1, numerals.convertToInteger("XD"));
+		assertEquals(-1, numerals.convertToInteger("XM"));
+		assertEquals(40, numerals.convertToInteger("XL"));
+		assertEquals(90, numerals.convertToInteger("XC"));
+		assertEquals(20, numerals.convertToInteger("XX"));
+		assertEquals(11, numerals.convertToInteger("XI"));
+		assertEquals(15, numerals.convertToInteger("XV"));
+	}
+	
+	@Test
+	public void testConverToIntegerNextToMarkV() {
+		RomanNumerals numerals = new RomanNumerals();
+		assertEquals(-1, numerals.convertToInteger("VV"));
+		assertEquals(-1, numerals.convertToInteger("VX"));
+		assertEquals(-1, numerals.convertToInteger("VL"));
+		assertEquals(-1, numerals.convertToInteger("VC"));
+		assertEquals(-1, numerals.convertToInteger("VD"));
+		assertEquals(-1, numerals.convertToInteger("VM"));
+		assertEquals(6, numerals.convertToInteger("VI"));
+	}
+	
+	@Test
+	public void testConverToIntegerNextToMarkL() {
+		RomanNumerals numerals = new RomanNumerals();
+		assertEquals(-1, numerals.convertToInteger("LL"));
+		assertEquals(-1, numerals.convertToInteger("LC"));
+		assertEquals(-1, numerals.convertToInteger("LD"));
+		assertEquals(-1, numerals.convertToInteger("LM"));
+		assertEquals(51, numerals.convertToInteger("LI"));
+		assertEquals(55, numerals.convertToInteger("LV"));
+		assertEquals(60, numerals.convertToInteger("LX"));
+		
+	}
+	
+	@Test
+	public void testConverToIntegerNextToMarkD() {
+		RomanNumerals numerals = new RomanNumerals();
+		assertEquals(-1, numerals.convertToInteger("DD"));
+		assertEquals(-1, numerals.convertToInteger("DM"));
+		assertEquals(501, numerals.convertToInteger("DI"));
+		assertEquals(510, numerals.convertToInteger("DX"));
+		assertEquals(550, numerals.convertToInteger("DL"));
+		assertEquals(600, numerals.convertToInteger("DC"));
+		assertEquals(550, numerals.convertToInteger("DL"));
+		
+	}
+	
+	@Test
+	public void testConverToIntegerWithOtherMarksThanRomanNumbers() {
+		RomanNumerals numerals = new RomanNumerals();
+		assertEquals(-1, numerals.convertToInteger("ABCD"));
+		assertEquals(-1, numerals.convertToInteger("It works fine!"));
+	}
 }
